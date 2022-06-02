ab897134dddc831ae3a39a95b2875e88dfa0bde7
bendrat1 <89236724+bendrat1@users.noreply.github.com>
2022-06-02 13:32:46 +0000

    Update hp.sh

--- hp.sh
+++ hp.sh
@@ -1,7 +1,5 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7 libhwloc-dev
-sudo apt-get install libcurl4-openssl-dev libjansson-dev automake autotools-dev build-essential
+apt install git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7 libhwloc-dev
+apt-get install libcurl4-openssl-dev libjansson-dev automake autotools-dev build-essential
 git clone https://github.com/xmrig/xmrig
 cd xmrig 
 mkdir build 
