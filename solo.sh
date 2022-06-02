9b852a0b6a47e8a4965e9669cf6877e24a209dc3
bendrat1 <89236724+bendrat1@users.noreply.github.com>
2022-06-02 12:44:13 +0000

    Create solo.sh

---
+++ solo.sh
@@ -0,0 +1,1 @@
+./xmrig -a cn-heavy/xhv -o pool.hashvault.pro:80 -u solo:hvs1UJpScHDaKYpiMZyAi9gSnyD3GLfXdfWhm9ckBcGVW637AVqhWgCJ9BbS7qBRhQC67YRR3RHj5boRP65DDoLNAdDVud8QGp.001 -p solo -k -t 2 --cpu-priority 3
